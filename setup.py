9d0dad19562359258757631c7b0e77ca275a3ee7
Beau Lyddon <beau.lyddon@webfilings.com>
2012-09-26 16:17:42 -0500

    Updating the rest of the setup.py configuration
    Adding the minimum to setup.py so we can package the plugin and
    push to pypi.

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 from setuptools import setup
 
+VERSION = '0.0.1'
 
 setup(
-    name="nose-gunit",
+    name="NoseGunit",
+    version=VERSION,
+    author='Beau Lyddon',
+    author_email='lyddonb@gmail.com',
+    maintainer='Beau Lyddon',
+    maintainer_email='lyddonb@gmail.com',
+    descrption='nose plugin for bootstrapping the GAE SDK and GAE Testbed',
+    url='https://github.com/beaulyddon-wf/nose-gunit',
+    license='Apache License 2.0',
+    classifiers=[
+        'Development Status :: 2 Pre-Alpha',
+        'License :: Apache License 2.0',
+        'Programming Language :: Python',
+        ],
     entry_points= {
         'nose.plugins.0.10':  [
             'nosegunit = nosegunit:NoseGunit'

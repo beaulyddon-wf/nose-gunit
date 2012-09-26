7c2631ec55e3c452a6543a4bd7f10cb0b74e44f3
Beau Lyddon <beau.lyddon@webfilings.com>
2012-09-26 16:01:27 -0500

    Adding proper module configuration to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
 
-setup(name="nose-gunit",
+setup(
+    name="nose-gunit",
     entry_points= {
         'nose.plugins.0.10':  [
             'nosegunit = nosegunit:NoseGunit'
             ]
-    })
+    },
+    py_modules=['nosegunit'],
+    install_requires=['nose>=0.10.1'],
+)
